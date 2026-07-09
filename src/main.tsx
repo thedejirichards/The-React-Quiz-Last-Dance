17554006617f2463cea5e02d5ab251fcc98e4ec9
macbook <macbook@macbooks-MacBook-Pro.local>
2026-07-09 08:43:45 +0100

    replaced the BankApp component with the App component in the main page

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-// import App from "./components/App.tsx";
-import BankApp from "./components/BankApp.tsx";
+import App from "./components/App.tsx";
+// import BankApp from "./components/BankApp.tsx";
 
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BankApp />
+    <App />
+    {/* <BankApp /> */}
   </StrictMode>,
 );
